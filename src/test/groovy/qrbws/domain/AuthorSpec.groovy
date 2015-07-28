d03e919d1a43a707101ec17dd384e4c0320ffee0
felansu <gaferran@gmail.com>
2015-07-27 23:55:26 -0300

    QRBWS-6  - Created Unit Test for Book & Author domains
     ISSUE: https://github.com/felansu/QRBWS/issues/6

--- src/test/groovy/qrbws/domain/AuthorSpec.groovy
+++ src/test/groovy/qrbws/domain/AuthorSpec.groovy
@@ -10,60 +10,60 @@ public class AuthorSpec extends Specification {
 
     void "Test that name must begin with an upper case letter"() {
         when: 'the name begins with a lower letter'
-        def p = new Author(name: 'ferran', notes: 'best author in 2009')
+        Author author = new Author(name: 'ferran', notes: 'best author in 2009')
 
         then: 'validation should pass'
-        p.validate()
+        author.validate()
 
         when: 'the name begins with an upper case letter'
-        p = new Author(name: 'Ferran', notes: 'best author of world')
+        author = new Author(name: 'Ferran', notes: 'best author of world')
 
         then: 'validation should pass'
-        p.validate()
+        author.validate()
     }
 
     void "Test that name don't can have numbers"() {
 
         when: 'the name have a number'
-        def p = new Author(name: 'ferran1989', notes: 'best author in 2009')
+        Author author = new Author(name: 'ferran1989', notes: 'best author in 2009')
 
         then: 'validation should fail'
-        !p.validate()
+        !author.validate()
     }
 
     void "Test notes can be null"() {
 
         when: 'notes be null'
-        def p = new Author(name: 'ferran', notes: null)
+        Author author = new Author(name: 'ferran', notes: null)
 
         then: 'validation should pass'
-        p.validate()
+        author.validate()
     }
 
     void "Test name can't be null"() {
 
         when: 'name be null'
-        def p = new Author(name: null)
+        Author author = new Author(name: null)
 
         then: 'validation should fail'
-        !p.validate()
+        !author.validate()
     }
 
     void "Test name can't be blank"() {
 
         when: 'name be blank'
-        def p = new Author(name: '')
+        Author author = new Author(name: '')
 
         then: 'validation should fail'
-        !p.validate()
+        !author.validate()
     }
 
     void "Test name can not exceed 254 characters"() {
 
         when: 'name be 255 characers'
-        def p = new Author(name: StringUtils.leftPad("", 255, '*'))
+        Author author = new Author(name: StringUtils.leftPad("", 255, '*'))
 
         then: 'validation should fail'
-        !p.validate()
+        !author.validate()
     }
 }
