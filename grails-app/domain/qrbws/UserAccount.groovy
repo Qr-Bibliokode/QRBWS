f0cc59a77d03e37c3078e0dac7438b11b14fe300
felansu <gaferran@gmail.com>
2015-08-27 20:47:09 -0300

    - Added function: A SMS is send when an user account is created

--- grails-app/domain/qrbws/UserAccount.groovy
+++ grails-app/domain/qrbws/UserAccount.groovy
@@ -10,6 +10,12 @@ class UserAccount {
     Person person
     Status status
 
+    def senderSMSController
+
+    def afterInsert() {
+        senderSMSController.sendSMSRegister(this)
+    }
+
     static constraints = {
         login blank: false, unique: true, size: 5..20
         password blank: false, size: 5..20
