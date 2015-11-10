328a2bf6d1f9968a1812e9ad87c996a2da256347
Ferran Gonzalez Alonso <ferran.zeroglosa@gmail.com>
2015-11-10 13:59:11 -0200

    - Atualizacao do boostrap.groovy

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -9,9 +9,12 @@ class BootStrap {
         new Idioma(descricao: 'Spanish').save();
         new Autor(nome: 'Aparecida', informacoesAdicionais: 'Beautiful pessoa').save();
         Estoque estoque = new Estoque(disponivel: 10, total: 10)
-        Livro livro = new Livro(isbn: "123456", titulo: "Livro teste", estoque: estoque).save()
-        Funcionario funcionario = new Funcionario(codigo: "1234", nome: "Ferran", email: "teste@teste.com").save()
-        ContaUsuario usuario = new ContaUsuario(pessoa: funcionario, username: "felansu", password: "123456").save()
+        new Livro(isbn: "123456", titulo: "Livro teste", estoque: estoque).save()
+        Pessoa funcionario = new Funcionario(codigo: "1234", nome: "Ferran", email: "felansu@teste.com").save()
+        ContaUsuario usuarioAdmin = new ContaUsuario(pessoa: funcionario, username: "felansu", password: "felansu").save()
+
+        Pessoa estudante = new Estudante(matricula: "4321", nome: "Aparecida", email: "aparecida@teste.com").save()
+        ContaUsuario usuarioEstudante = new ContaUsuario(pessoa: estudante, username: "aparecida", password: "aparecida").save()
 
         Livro.marshaller()
         Comentario.marshaller()
@@ -26,7 +29,8 @@ class BootStrap {
         Role usuarioRol = new Role('ROLE_USER').save()
 
 
-        ContaUsuarioRole.create usuario, adminRole, true
+        ContaUsuarioRole.create usuarioAdmin, adminRole, true
+        ContaUsuarioRole.create usuarioEstudante, usuarioRol, true
 
     }
     def destroy = {
